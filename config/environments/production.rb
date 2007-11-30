1608445e3b6acdabb16864225a51729e02a61aaa
peeoutside <peeoutside@f439cc64-dcae-62a1-c29a-ba80cfb482d8>
2007-11-30 21:11:48 +0000

    changed production sendmail settings
    git-svn-id: svn+ssh://8.17.86.51/users/home/peeoutside/svn/repo@15 f439cc64-dcae-62a1-c29a-ba80cfb482d8

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,7 @@ config.action_controller.perform_caching             = true
 config.action_mailer.delivery_method = :sendmail
 ActionMailer::Base.delivery_method = :sendmail
 ActionMailer::Base.sendmail_settings = {
-  :location       => '/opt/csw/sbin/sendmail',
+  :location       => '/usr/local/bin/sendmail',
   :arguments      => '-i -t -f gluedtomyseat@gmail.com
 }
 config.action_mailer.default_charset = "utf-8"
