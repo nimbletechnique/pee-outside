81c1d4786cd250ea237310fd1cee10b0044eebc8
Collin VanDyck <gluedtomyseat@gmail.com>
2008-07-18 14:15:00 -0400

    Updated deployment for newer mysql

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,24 +1,31 @@
-set :runner, 'peeoutside'
 set :application, "peeoutside"
-set :use_sudo, false
-set :checkout, "export"
+set :user, "collin"
+set :host, "#{user}@peeoutside.org"
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :scm, :git
+set :repository, "git@github.com:oculardisaster/pee-outside.git"
 
-set :deploy_to, "/users/home/peeoutside/rails/peeoutside"
+set :deploy_to, "/var/www/apps/#{application}"
+set :runner, user
+set :ssh_options, { :forward_agent => true }
+set :branch, "master"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+role :app, "#{host}"
+role :web, "#{host}"
+role :db,  "#{host}", :primary => true
 
-# repo is localhost on this deployment
-set :repository,  "svn+ssh://peeoutside@8.17.86.51/home/peeoutside/svn/repo"
-set :svn_user, 'peeoutside'
-set :svn_password, 'p330uts1d3'
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
 
-role :app, 'peeoutside@peeoutside.org'
-role :web, 'peeoutside@peeoutside.org'
-role :db,  'peeoutside@peeoutside.org', :primary => true
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app do ; end
+  end
+end
+
+after 'deploy:finalize_update' do
+  # run "cp #{deploy_to}/shared/database.yml #{latest_release}/config"
+end
